3c08e13d72dd0db2c7f3de8ffe31a0bc8c6b531f
kstratton2 <kennystratton81@yahoo.com>
2016-03-14 17:53:19 -0500

    Basics of Node class
    add constructor
    add variables
    add getters
    add setters
    add JavaDoc

--- src/edu/unl/cse/Sudoku/ui/Node.java
+++ src/edu/unl/cse/Sudoku/ui/Node.java
@@ -2,4 +2,44 @@ package edu.unl.cse.Sudoku.ui;
 
 public class Node {
 
+	private Node nextNode;
+	private Change change;
+	
+	public Node(Node next, Change lastChange){
+		this.nextNode = next;
+		this.change = lastChange;
+	}
+
+	/**
+	 * Getter for the next node in the stack
+	 * @return
+	 */
+	public Node getNextNode() {
+		return nextNode;
+	}
+
+	/**
+	 * Setter for the next node variable of this node
+	 * @param nextNode
+	 */
+	public void setNextNode(Node nextNode) {
+		this.nextNode = nextNode;
+	}
+
+	/**
+	 * Getter for the change variable of this node
+	 * @return
+	 */
+	public Change getChange() {
+		return change;
+	}
+
+	/**
+	 * Setter for the change variable of this node
+	 * @param change
+	 */
+	public void setChange(Change change) {
+		this.change = change;
+	}
+	
 }
