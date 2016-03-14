59a232b811434f8eb2f9fb70ddaa37a69015bf74
chrislundquist14 <chrislundquist14@gmail.com>
2016-03-14 16:24:12 -0600

    Change
    added the getters and setters for Change

--- src/edu/unl/cse/Sudoku/ui/Change.java
+++ src/edu/unl/cse/Sudoku/ui/Change.java
@@ -1,5 +1,32 @@
 package edu.unl.cse.Sudoku.ui;
 
 public class Change {
+int prevVal;
+int row;
+int col;
 
+public void setPrevVal(int prev)
+{
+	prevVal = prev;
+}
+public void setRow(int r)
+{
+	row = r;
+}
+public void setCol(int c)
+{
+	col = c;
+}
+public int getPrevVal()
+{
+	return prevVal;
+}
+public int getRow()
+{
+	return row;
+}
+public int getCol()
+{
+	return col;
+}
 }
