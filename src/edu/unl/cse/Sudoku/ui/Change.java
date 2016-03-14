bebeb03a5aa513cba03ea5f81f2e92596e3bd1b5
kstratton2 <kennystratton81@yahoo.com>
2016-03-14 17:44:30 -0500

    Edit Change class
    add JavaDocs
    add constructor
    change variable permissions

--- src/edu/unl/cse/Sudoku/ui/Change.java
+++ src/edu/unl/cse/Sudoku/ui/Change.java
@@ -1,32 +1,62 @@
 package edu.unl.cse.Sudoku.ui;
 
 public class Change {
-int prevVal;
-int row;
-int col;
+	
+	private int prevVal;
+	private int row;
+	private int col;
+	
+	public Change(int prevVal, int r, int c){
+		this.prevVal = prevVal;
+		this.row = r;
+		this.col = c;
+	}
+	
+	/**
+	 * Setter for the previous value variable of the Block change
+	 * @param prev
+	 */
+	public void setPrevVal(int prev) {
+		this.prevVal = prev;
+	}
 
-public void setPrevVal(int prev)
-{
-	prevVal = prev;
-}
-public void setRow(int r)
-{
-	row = r;
-}
-public void setCol(int c)
-{
-	col = c;
-}
-public int getPrevVal()
-{
-	return prevVal;
-}
-public int getRow()
-{
-	return row;
-}
-public int getCol()
-{
-	return col;
-}
+	/**
+	 * Setter for the row in which the Changed Block lives
+	 * @param r
+	 */
+	public void setRow(int r) {
+		this.row = r;
+	}
+
+	/**
+	 * Setter for the column in which the changed Block lives
+	 * @param c
+	 */
+	public void setCol(int c) {
+		this.col = c;
+	}
+
+	/**
+	 * Getter for the previous value of the changed Block
+	 * @return
+	 */
+	public int getPrevVal() {
+		return this.prevVal;
+	}
+
+	/**
+	 * Getter for the row of the changed Block
+	 * @return
+	 */
+	public int getRow() {
+		return this.row;
+	}
+
+	/**
+	 * Getter for the column of the changed Block
+	 * @return
+	 */
+	public int getCol() {
+		return this.col;
+	}
 }
