66dfd013c4c397c30bfded1e7438dcddde1dab6b
kstratton2 <kennystratton81@yahoo.com>
2016-03-14 17:22:41 -0500

    Add basic properties and functions to Block

--- src/edu/unl/cse/Sudoku/ui/Block.java
+++ src/edu/unl/cse/Sudoku/ui/Block.java
@@ -2,4 +2,63 @@ package edu.unl.cse.Sudoku.ui;
 
 public class Block {
 
+	private int value;
+	private boolean editable;
+	private int[] notes= {};
+	
+	public Block(int value, boolean editable){
+		this.value = value;
+		this.editable = editable;
+	}
+	
+	/**
+	 * Getter for the value of the Block
+	 * @return value
+	 */
+	int getValue(){
+		return this.value;
+	}
+	
+	/**
+	 * Setter for the value of the Block
+	 * @param newVal
+	 */
+	void setValue(int newVal){
+		this.value = newVal;
+	}
+	
+	/**
+	 * Getter for the notes of this block
+	 * @return notes
+	 */
+	int[] getNotes(){
+		return this.notes;
+	}
+	
+	/**
+	 * Method to add an integer to the list of notes
+	 * for this Block
+	 * @param note
+	 */
+	void addNote(int note){
+		//TODO:
+		
+	}
+	
+	/**
+	 * Method to remove an integer from the list of notes
+	 * for this Block
+	 * @param note
+	 */
+	void removeNote(int note){
+		//TODO:
+		
+	}
+	
+	/**
+	 * Method to remove ALL notes from a Block
+	 */
+	void clearNotes(){
+		
+	}
 }
